873f0da282eb9f40e6a5fc2df0f14a3ff1197949
Dmivtr <dmivtr@gmail.com>
2018-07-09 17:12:24 +0300

    changing brain-games.js

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
-<<<<<<< HEAD
 import { whatIsUserName } from '..';
-=======
-import whatIsUserName from '..';
->>>>>>> a44ce3f0c739ccf5de6aa7f11036e0dbcde46f20
 
 console.log('Welcome to the Brain Games!');
 whatIsUserName();
