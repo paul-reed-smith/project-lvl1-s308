acb74cff5a4262439a8f94f59db2468e088657b1
Dmivtr <dmivtr@gmail.com>
2018-07-10 15:05:24 +0300

    small fix 2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const welcomeUser = () => {
   console.log('Welcome to Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello! ${userName}`);
+  console.log(`Hello! ${userName} :)`);
 };
 
 export default welcomeUser;
