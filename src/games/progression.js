b7a14a87ca4562aa525eb17f8e9aaa127c63aa62
Dmivtr <dmivtr@gmail.com>
2018-07-15 18:06:22 +0300

    add brain-progression

---
+++ src/games/progression.js
@@ -0,0 +1,38 @@
+import { cons } from 'hexlet-pairs';
+import getRandomNum from '../utils';
+import gameEngine from '../game-engine';
+
+const gameTask = 'What number is missing in this progression?';
+
+const beginOfRange = 1;
+const endOfRange = 100;
+
+const getGameLogic = () => {
+  const numberOfProgressionItems = 10;
+
+  const minFirstProgressionItem = 1;
+  const maxFirstProgressionItem = 100;
+  const firstProgressionItem = getRandomNum(minFirstProgressionItem, maxFirstProgressionItem);
+  const stepOfProgression = getRandomNum(beginOfRange, endOfRange);
+
+  const minHiddenItemNumber = 2;
+  const maxHiddenItemNumber = 10;
+  const hiddenItemNumber = getRandomNum(minHiddenItemNumber, maxHiddenItemNumber);
+
+  let res = '';
+  let progressionItem = firstProgressionItem;
+  let correctAnswer = 0;
+  for (let i = 0; i <= numberOfProgressionItems; i += 1) {
+    progressionItem += stepOfProgression;
+    if (i !== hiddenItemNumber) {
+      res += `${progressionItem} `;
+    } else {
+      res += '.. ';
+      correctAnswer = progressionItem;
+    }
+  }
+  const question = res;
+
+  return cons(question, correctAnswer);
+};
+export default () => gameEngine(gameTask, getGameLogic);
