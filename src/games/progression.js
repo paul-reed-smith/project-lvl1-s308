f3bff102ff2dc285337f5d2ed8c3c8a35b97ae7f
Dmivtr <dmivtr@gmail.com>
2018-07-15 18:12:13 +0300

    small fix progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -7,16 +7,17 @@ const gameTask = 'What number is missing in this progression?';
 const beginOfRange = 1;
 const endOfRange = 100;
 
+const minFirstProgressionItem = 1;
+const maxFirstProgressionItem = 100;
+
+const minHiddenItemNumber = 2;
+const maxHiddenItemNumber = 10;
+
 const getGameLogic = () => {
   const numberOfProgressionItems = 10;
-
-  const minFirstProgressionItem = 1;
-  const maxFirstProgressionItem = 100;
   const firstProgressionItem = getRandomNum(minFirstProgressionItem, maxFirstProgressionItem);
   const stepOfProgression = getRandomNum(beginOfRange, endOfRange);
 
-  const minHiddenItemNumber = 2;
-  const maxHiddenItemNumber = 10;
   const hiddenItemNumber = getRandomNum(minHiddenItemNumber, maxHiddenItemNumber);
 
   let res = '';
